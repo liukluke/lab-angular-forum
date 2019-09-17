c34617a3290cddafe81eff4a75960e74330784c4
liukluke <luca.manfredi1987@gmail.com>
2019-09-17 13:40:54 +0200

    login html

--- starter-code/client/src/app/components/auth/login/login.component.ts
+++ starter-code/client/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../../services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./../../../styles/style.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  error: string;
+  newLoginInput: Object = {
+    username: '',
+    password: ''
+  };
+  user: Object;
+  constructor(private session: AuthService, private router: Router) { }
+  ngOnInit() { }
+  login(): void {
+    this.session.login(this.newLoginInput)
+      .subscribe(
+        (user: HttpClient) => {
+          this.user = user;
+          this.session.isUserLoggedIn.next(true);
+          this.newLoginInput = {};
+          this.router.navigate(['']);
+        },
+        (err: any) => this.error = err
+      );
   }
-
 }
--- starter-code/client/src/app/services/auth.service.ts
+++ starter-code/client/src/app/services/auth.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
+import { environment } from '../../environments/environment'
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthService {
+  options: object = { withCredentials: true };
+  public isUserLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  handleError(e: any): Observable<never> {
+    return throwError(e.message);
+  }
+
+  signup(user: any): Observable<HttpClient> {
+    return this.httpClient.post(`${environment.BASE_URL}/signup`, user, this.options)
+      .pipe(
+        map((data: any) => data),
+        catchError(this.handleError)
+      );
+  }
+
+  login(user: any) {
+    return this.httpClient.post(`${environment.BASE_URL}/login`, user, this.options)
+      .pipe(
+        map((data: any) => data),
+        catchError(this.handleError)
+      );
+  }
+
+  logout(): Observable<HttpClient> {
+    return this.httpClient.post(`${environment.BASE_URL}/logout`, {}, this.options)
+      .pipe(
+        map((data: any) => data),
+        catchError(this.handleError)
+      );
+  }
+
+  isLoggedIn(): Observable<any> {
+    return this.httpClient.get(`${environment.BASE_URL}/loggedin`, this.options)
+      .pipe(
+        map((data: any) => data),
+        catchError(this.handleError)
+      );
+  }
+
 }
